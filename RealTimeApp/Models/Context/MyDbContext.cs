1d061939b2fdf84e2870808e56af953eac0ba3dc
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-10-07 15:33:37 +0200

    AddingDBContext

---
+++ RealTimeApp/Models/Context/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealTimeApp.Models.Context
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Notification> Notification { get; set; }
+    }
+}
