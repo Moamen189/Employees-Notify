974976404e47d808ddb47eb9e0146b71d54e99c0
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-10-07 15:46:25 +0200

    MaintainEFCodeFirst

---
+++ RealTimeApp/Data/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealTimeApp.Models;
+
+namespace RealTimeApp.Data
+{
+   
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext(DbContextOptions<MyDbContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Employee> Employee { get; set; }
+            public DbSet<Notification> Notification { get; set; }
+        }
+    
+}
